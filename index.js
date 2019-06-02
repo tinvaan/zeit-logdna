8ae99c9b38e3522be9f71111a79543e9adadb6e0
Harish Navnit <harishnavnit@gmail.com>
2019-06-02 16:46:42 +0530

    Remove bloat from launch logic

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 'use strict'
 
 const program = require('commander')
-const { withUiHook, htm } = require('@zeit/integration-utils')
+const { htm, withUiHook } = require('@zeit/integration-utils')
 
 const logs = require('./src/logs')
-
-const format = async (deploy) => {
-  const events = await logs.fetch(deploy)
-  const formatted = await logs.format(events.data)
-
-  return formatted
-}
-
-const transport = async (deploy) => {
-  const events = await logs.fetch(deploy)
-  const formatted = await logs.format(events.data)
-
-  const response = await logs.relay(formatted)
-  return response
-}
 
 let count = 0
 module.exports = withUiHook(({ payload }) => {
@@ -35,18 +20,30 @@ program
   .version('1.0.0')
 
 program
+  .command('projects')
+  .option('--list', 'list all projects')
+  .action(async (action) => {
+    if (action.list) {
+      const projects = await Projects.list()
+      console.log(projects.data)
+    }
+  })
+
+program
   .command('logs <deployment>')
   .option('--list', 'list deployments')
   .option('--fetch', 'list deployment ')
   .option('--export', 'export logs to LogDNA')
   .option('--format', 'format logs to LogDNA format')
   .action(async (deploy, action) => {
-    if (action.export) {
-      const exported = await transport(deploy); console.log(exported)
-    } else if (action.format) {
-      const formatted = await format(deploy); console.log(formatted)
-    } else if (action.list || action.fetch) {
-      const events = await logs.fetch(deploy); console.log(events.data)
+    const events = await logs.fetch(deploy)
+    const formatted = await logs.format(events.data)
+
+    if (action.format) console.log(formatted)
+    else if (action.list || action.fetch) console.log(events.data)
+    else if (action.export) {
+      const exported = await logs.relay(formatted)
+      console.log(exported)
     }
   })
 
